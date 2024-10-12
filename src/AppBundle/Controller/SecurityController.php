69af679c7496caae27f3b71b0219817ac7c6973c
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 21:02:37 +0400

    Update SecurityController.php

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -1,5 +1,4 @@
 <?php
-// assets/AppBundle/Controller/SecurityController.php
 
 namespace AppBundle\Controller;
 
@@ -8,41 +7,23 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class SecurityController extends Controller
 {
-    /**
-     * @Route("/login", name="login-route")
-     */
     public function loginAction()
     {
         $authenticationUtils = $this->get('security.authentication_utils');
-
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render(
             'AppBundle:Security:login.html.twig',
             array(
-                // last username entered by the user
                 'last_username' => $lastUsername,
                 'error'         => $error,
             )
         );
     }
-
-    /**
-     * @Route("/login-check", name="login-check")
-     */
     public function loginCheckAction()
     {
-        // this controller will not be executed,
-        // as the route is handled by the Security system
     }
-
-    /**
-     * @Route("/logout", name="logout")
-     */
     public function logoutAction()
     {
 
