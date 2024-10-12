203eb46c735ea311cf0b4e06ab41990ce0e35148
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 21:21:49 +0400

    Update app.php

--- web/app.php
+++ web/app.php
@@ -5,10 +5,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
 $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
 $apcLoader->register(true);
@@ -18,7 +14,6 @@ require_once __DIR__.'/../app/AppKernel.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
