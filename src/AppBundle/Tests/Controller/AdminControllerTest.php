a326d61393dea98c2389b1fe56ccb40505df3030
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 21:07:27 +0400

    Update AdminControllerTest.php

--- src/AppBundle/Tests/Controller/AdminControllerTest.php
+++ src/AppBundle/Tests/Controller/AdminControllerTest.php
@@ -4,19 +4,10 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use AppBundle\Tests\Util\AuthenticatedClient;
-
-/**
- * Class AdminControllerTest
- * @package AppBundle\Tests\Controller
- *
- * @see http://stackoverflow.com/questions/19015385/how-to-log-in-user-in-session-within-a-functional-test-in-symfony-2-3
- * @see http://symfony.com/doc/2.8/cookbook/testing/http_authentication.html
- */
 class AdminControllerTest extends WebTestCase
 {
     private $client = null;
     private $container = null;
-
     public function setUp()
     {
         $this->client = AuthenticatedClient::login();
