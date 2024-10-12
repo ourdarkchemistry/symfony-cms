82e960ef9b4bbfe0412241b0ed11a10d66f47f2a
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 21:01:19 +0400

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,12 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="home")
-     */
     public function indexAction()
     {
-        // replace this example code with whatever you need
         $helloWorld = $this->get('translator')->trans('Hello World!');
         return $this->render('default/index.html.twig', array('helloWorld' => $helloWorld));
     }
