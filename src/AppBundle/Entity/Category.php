6e37bc8bd4dba1f49a5377494818ebc87e62b860
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 21:03:50 +0400

    Update Category.php

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -4,36 +4,12 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
-
-/**
- * Category
- *
- * @ORM\Table()
- * @ORM\Entity
- */
 class Category
 {
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
     private $id;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=50)
-     */
     private $name;
-
-    /**
-     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Page", mappedBy="category")
-     */
     private $pages;
-
+    
     public function __construct()
     {
         $this->pages = new ArrayCollection();
@@ -43,71 +19,30 @@ class Category
     {
         return $this->name;
     }
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
     public function getId()
     {
         return $this->id;
     }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return Category
-     */
     public function setName($name)
     {
         $this->name = $name;
 
         return $this;
     }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
     public function getName()
     {
         return $this->name;
     }
-
-
-    /**
-     * Add page
-     *
-     * @param \AppBundle\Entity\Page $page
-     *
-     * @return Category
-     */
     public function addPage(\AppBundle\Entity\Page $page)
     {
         $this->pages[] = $page;
 
         return $this;
     }
-
-    /**
-     * Remove page
-     *
-     * @param \AppBundle\Entity\Page $page
-     */
     public function removePage(\AppBundle\Entity\Page $page)
     {
         $this->pages->removeElement($page);
     }
-
-    /**
-     * Get pages
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
     public function getPages()
     {
         return $this->pages;
