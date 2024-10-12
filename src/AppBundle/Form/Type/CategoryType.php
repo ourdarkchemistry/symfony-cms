e5240845e00ee42c021c3522cbf8045ef1179b05
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 21:05:58 +0400

    Update CategoryType.php

--- src/AppBundle/Form/Type/CategoryType.php
+++ src/AppBundle/Form/Type/CategoryType.php
@@ -8,30 +8,18 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class CategoryType extends AbstractType
 {
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
         ;
     }
-    
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\Category'
         ));
     }
-
-    /**
-     * @return string
-     */
     public function getName()
     {
         return 'appbundle_category';
