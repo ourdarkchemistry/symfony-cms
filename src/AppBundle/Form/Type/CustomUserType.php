90741be24591ea5a9d117e6f58509175f6ab72c4
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 21:06:15 +0400

    Update CustomUserType.php

--- src/AppBundle/Form/Type/CustomUserType.php
+++ src/AppBundle/Form/Type/CustomUserType.php
@@ -8,10 +8,6 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class CustomUserType extends AbstractType
 {
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -19,20 +15,12 @@ class CustomUserType extends AbstractType
             ->add('password', 'password')
         ;
     }
-    
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\CustomUser'
         ));
     }
-
-    /**
-     * @return string
-     */
     public function getName()
     {
         return 'appbundle_customuser';
