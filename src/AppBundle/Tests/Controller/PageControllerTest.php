858e85399f49cdab140a0dca88fc121a2f591819
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 21:09:17 +0400

    Update PageControllerTest.php

--- src/AppBundle/Tests/Controller/PageControllerTest.php
+++ src/AppBundle/Tests/Controller/PageControllerTest.php
@@ -16,7 +16,6 @@ class PageControllerTest extends WebTestCase
 
     public function testCompleteScenario()
     {
-        // Create a new entry in the database
         $crawler = $this->client->request('GET', '/cms/page/');
         $this->assertEquals(200,
             $this->client->getResponse()->getStatusCode(),
@@ -25,8 +24,6 @@ class PageControllerTest extends WebTestCase
         $crawler = $this->client->click(
             $crawler->selectLink('Create a new entry')->link()
         );
-
-        // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
             'appbundle_page[title]'  => 'Test title',
             'appbundle_page[content]' => 'Test content'
@@ -34,13 +31,10 @@ class PageControllerTest extends WebTestCase
 
         $this->client->submit($form);
         $crawler = $this->client->followRedirect();
-
-        // Check data in the show view
+        
         $this->assertGreaterThan(0,
             $crawler->filter('td:contains("Test title")')->count(),
             'Missing element td:contains("Test title")');
-
-        // Edit the entity
         $crawler = $this->client->click($crawler->selectLink('Edit')->link());
 
         $form = $crawler->selectButton('Update')->form(array(
@@ -51,15 +45,12 @@ class PageControllerTest extends WebTestCase
         $this->client->submit($form);
         $crawler = $this->client->followRedirect();
 
-        // Check the element contains an attribute with value equals "Foo"
         $this->assertGreaterThan(0,
             $crawler->filter('[value="Test title update"]')->count(),
             'Missing element [value="Test title update"]');
 
-        // Delete the entity
         $this->client->submit($crawler->selectButton('Delete')->form());
-
-        // Check the entity has been delete on the list
+        
         $this->assertNotRegExp('/Test+title+update/',
             $this->client->getResponse()->getContent());
     }
